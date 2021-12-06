7ceea70fec6e7af4413d8a18e8592524d7bbc82f
Eli <elisego46@gmail.com>
2021-12-06 16:59:35 -0600

    Separate implementation with header

--- Database.cpp
+++ Database.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Database.h"
+
+Database::Database(int k, bool nonzero) {
+    for (int i = 0; i < k; i++)
+        if (nonzero)
+            data.push_back(i+1);
+        else
+            data.push_back(0);
+}
+int Database::Read(int k) {
+    return data[k];
+}
+void Database::Write(int k, int w) {
+    data[k] = w;
+}
+void Database::Print() {
+    for (int i = 0; i < data.size(); i++)
+        cout << data[i] << " ";
+}
--- Database.h
+++ Database.h
@@ -11,23 +11,12 @@
 using namespace std;
 class Database {
 public:
-    Database(int k, bool nonzero) {
-        for (int i = 0; i < k; i++)
-            if (nonzero)
-                data.push_back(i+1);
-            else
-                data.push_back(0);
-    }
-    int Read(int k) {
-        return data[k];
-    }
-    void Write(int k, int w) {
-        data[k] = w;
-    }
-    void Print() {
-        for (int i = 0; i < data.size(); i++)
-            cout << data[i] << " ";
-    }
+    Database(int k, bool nonzero);
+
+
+    int Read(int k);
+    void Write(int k, int w);
+    void Print();
 private:
     vector<int> data;
 };
